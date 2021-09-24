c17e43543fe0d98531f29b1bcd46dc325c87e0c3
Krzysztof Sakrejda <sakrejda@umich.edu>
2021-09-24 10:48:18 -0400

    Need to set directory from workers for git ocmmands, maybe.

--- R/helpers.R
+++ R/helpers.R
@@ -1,6 +1,8 @@
 timestamp = function() Sys.time() %>% stringr::str_replace(' ', '--')
 
-repo_hash = function() system2(command = "git", args = "log -n1 --format=%H")
+repo_hash = function(
+  root = workflow::project_dir()
+) system2(command = "git", args = glue::glue("-C {root} log -n1 --format=%H"))
 
 
 
